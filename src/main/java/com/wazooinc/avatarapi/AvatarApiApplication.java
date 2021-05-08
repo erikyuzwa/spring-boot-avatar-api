ddf422e61734858c075d3d4f1735b5a691598d19
erik yuzwa <erikyuzwa@gmail.com>
2021-05-07 18:59:19 -0600

    initial commit

---
+++ src/main/java/com/wazooinc/avatarapi/AvatarApiApplication.java
@@ -0,0 +1,13 @@
+package com.wazooinc.avatarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvatarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvatarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wazooinc/avatarapi/AvatarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wazooinc.avatarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvatarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
